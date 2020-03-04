e7b70380c7f27b8dcd189a8904b21ad48eb3af5a
Brad Phillips <bjrp@protonmail.com>
2020-03-04 16:05:16 +0000

    Passes withdrawal tests: can handle withdrawal; cannot receive negative withdrawal; cannot go into negative balance

--- spec/transactionsSpec.js
+++ spec/transactionsSpec.js
@@ -18,4 +18,20 @@ describe('Transactions', function() {
       expect(ac.getBalance()).toEqual(0);
     });
   });
+
+  describe('Make a withdrawal', function() {
+    it('Can handle a withdrawal', function() {
+      deposit(ac, 100);
+      withdraw(ac, 50);
+      expect(ac.getBalance()).toEqual(50);
+    });
+    it('Cannot receive a negative withdrawal', function() {
+      withdraw(ac, -10);
+      expect(ac.getBalance()).toEqual(0);
+    });
+    it('Cannot receive a withdrawal if balance will be below zero', function() {
+      withdraw(ac, 10);
+      expect(ac.getBalance()).toEqual(0);
+    });
+  });
 })
--- src/transactions.js
+++ src/transactions.js
@@ -2,6 +2,14 @@ function deposit(account, amount){
   if (amount < 0) {
     return account.balance
   }
-  
+
   return account.balance += amount
+}
+
+function withdraw(account, amount) {
+  if (amount > account.balance || amount < 0) {
+    return account.balance
+  }
+
+  return account.balance -= amount
 }
