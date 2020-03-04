898b4438e6b83dad8de37db30c14753a359fd7ad
Brad Phillips <bjrp@protonmail.com>
2020-03-04 21:43:55 +0000

    Passes transaction history test

--- spec/bankAccountSpec.js
+++ spec/bankAccountSpec.js
@@ -15,8 +15,10 @@ describe('Bank Account', function() {
   });
   describe('Transaction history', function(){
     it('Records a deposit', function(){
+      var today = (new Date).toLocaleString().slice(0,10)
       deposit(ac, 10)
       expect(ac.transaction_history.length).toEqual(1);
+      expect(ac.transaction_history[0]).toEqual({date: today, deposit: 10, withdrawal: 0, balance: 10});
     })
   })
 })
--- src/transactions.js
+++ src/transactions.js
@@ -2,7 +2,13 @@ function deposit(account, amount){
   if (amount < 0) {
     return account.balance
   }
-   account.balance += amount
+
+  var new_balance = account.balance += amount
+
+  var today = (new Date).toLocaleDateString()
+  var transaction = {date: today, deposit: amount, withdrawal: 0, balance: new_balance }
+  account.transaction_history.push(transaction)
+  new_balance
 }
 
 function withdraw(account, amount) {
