14a944280b521592f37c8fd3257d1b87c73f2297
Brad Phillips <bjrp@protonmail.com>
2020-03-05 16:35:15 +0000

    Passes test: record withdrawal; refactors withdrawal method

--- spec/bankAccountSpec.js
+++ spec/bankAccountSpec.js
@@ -3,9 +3,11 @@
 describe('Bank Account', function() {
 
   var ac;
+  var today;
 
   beforeEach(function(){
     ac = new Account();
+    today = (new Date).toLocaleString().slice(0,10);
   })
 
   describe('Create new account', function() {
@@ -15,10 +17,15 @@ describe('Bank Account', function() {
   });
   describe('Account history', function(){
     it('Records a deposit', function(){
-      var today = (new Date).toLocaleString().slice(0,10)
       deposit(ac, 10)
       expect(ac.transaction_history.length).toEqual(1);
       expect(ac.transaction_history[0]).toEqual({date: today, deposit: 10, withdrawal: 0, balance: 10});
+    })
+    it('Records a withdrawal', function(){
+      deposit(ac, 10)
+      withdraw(ac, 10)
+      expect(ac.transaction_history.length).toEqual(2);
+      expect(ac.transaction_history[1]).toEqual({date: today, deposit: 0, withdrawal: 10, balance: 0});
     })
   })
 })
--- src/transactions.js
+++ src/transactions.js
@@ -1,22 +1,33 @@
 function deposit(account, amount){
   if (amount < 0) {
-    return account.balance
+    console.log('Please enter a positive number.');
+    return account.balance;
   }
-
   var new_balance = account.balance += amount
-  record_transaction(account, amount, new_balance)
-  new_balance
-}
-
-function record_transaction(account, amount, new_balance) {
-  var today = (new Date).toLocaleDateString()
-  var transaction = {date: today, deposit: amount, withdrawal: 0, balance: new_balance }
-  account.transaction_history.push(transaction)
+  recordDeposit(account, amount, new_balance)
 }
 
 function withdraw(account, amount) {
   if (amount > account.balance || amount < 0) {
+    
     return account.balance
   }
-  account.balance -= amount
+  var new_balance = account.balance -= amount
+  recordWithdrawal(amount, new_balance, account);
+}
+
+function recordWithdrawal(amount, new_balance, account) {
+  var today = (new Date).toLocaleDateString();
+  var transaction = { date: today, deposit: 0, withdrawal: amount, balance: new_balance };
+  recordTransaction(account, transaction)
+}
+
+function recordDeposit(account, amount, new_balance) {
+  var today = (new Date).toLocaleDateString()
+  var transaction = {date: today, deposit: amount, withdrawal: 0, balance: new_balance }
+  recordTransaction(account, transaction)
+}
+
+function recordTransaction(account, transaction) {
+  account.transaction_history.push(transaction);
 }
